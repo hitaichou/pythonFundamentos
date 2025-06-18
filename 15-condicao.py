b31fe8ab10648f9d10ff3f413ff04b7d18383fb9
Raphael Fernandes <fernandes.raphael@live.com>
2025-06-18 06:04:55 -0300

    Exemplo de if no python

---
+++ 15-condicao.py
@@ -0,0 +1,34 @@
+#informacoes sobre filmes
+# name = input("Digite o nome do filme:\n ")
+# yearRelease = int(input("Digite o ano de lançamento:\n "))
+# rating = float(input("Digite a nota de avaliação do filme:\n "))
+
+# #Verifica se o filme é recomendado
+# if rating > 8.0 and yearRelease > 2015:
+#     print(f"O filme {name} é muito bom. Recomendado assistí-lo!")
+# else:
+#     print(f"O filme {name} ainda não atingiu uma boa nota")
+
+
+num1 = float(input("Digite o primeiro número:\n "))
+num2 = float(input("Digite o segundo número:\n "))
+operation = input("Digite a operação a ser realizada(+, -, *, /):\n ")
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        print("Err0o: Divisão por zero.")
+        result = 0
+else:
+    print("Operação inválida.")
+    result = 0
+
+print(f"O resultado da operação: {result:.2f} ")
+            
